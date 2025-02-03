9af013f4a7c4170108b54479eee2b533abab4fa7
gor.barsegyan <92037197+Frogleim@users.noreply.github.com>
2025-02-03 16:59:59 +0400

    update

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
+from os import PRIO_PGRP
+
 from exchanges import mexc_exchange, bingx_exchange
 
 signal_example = {
     "symbol": "ETHUSDT",
     "network": "BEP20",
     "quantity": 1,
-    "price": 3270,
-    "exchange": "MEXC"
+    "price": 2608,
+    "exchange": "BINGX"
 }
 def get_signal():
     if signal_example['exchange'] == 'MEXC':
@@ -15,6 +17,8 @@ def get_signal():
         if data['is_near']:
             print("Signal is valid! Starting arbitrage")
             return True, 'MEXC'
+        else:
+            return False, 'MEXC'
 
     elif signal_example['exchange'] == 'BINGX':
         bingx = bingx_exchange.BingX()
@@ -22,6 +26,8 @@ def get_signal():
         if data['is_near']:
             print("Signal is valid! Starting arbitrage")
             return True, 'BINGX'
+        else:
+            return False, 'BINGX'
 
 def start_arbitrage():
     mex = mexc_exchange.Mexc()
@@ -29,22 +35,30 @@ def start_arbitrage():
     bingx = bingx_exchange.BingX()
     is_valid, exchange = get_signal()
     if is_valid and exchange == 'MEXC':
-        addr = mex.get_asset_address(
+        addr = bingx.get_asset_address(
             coin=coin,
             network=signal_example['network']
         )
-        print(addr)
+        contract = next((item['contractAddress'] for item in addr if item['network'] == signal_example['network']), None)
+
+        print(f'BINGX {coin} address: {contract}')
+
+        print(f"Buying {signal_example['symbol']} with quantity {signal_example['quantity']} on BINGX")
         # order = mex.buy_crypto(
         #     coin=signal_example['symbol'],
         #     amount=signal_example['quantity']
         # )
         # print(order)
-        print(f"Buying {signal_example['symbol']} with quantity {signal_example['quantity']} on MEXC")
-    elif is_valid and exchange == 'MEXC':
-        addr = bingx.get_asset_address(
+        print(f'Sending crypto to BING address')
+        bingx.sell_crypto()
+        print('Finishing')
+
+    elif is_valid and exchange == 'BINGX':
+        addr = mex.get_asset_address(
             coin=coin,
             network=signal_example['network']
         )
+        print(addr)
         print(f"Buying {signal_example['symbol']} with quantity {signal_example['quantity']} on MEXC")
 
 
